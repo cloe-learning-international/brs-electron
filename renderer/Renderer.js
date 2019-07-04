68ae5dcbaeb209ed4b2f2978d008846e6b9e28c3
Benjamin Roseau <roseaubenjamin@gmail.com>
2019-07-04 10:24:21 +0200

    Suppression et désinstallation des push notification, on verra plus tard

--- renderer/Renderer.js
+++ renderer/Renderer.js
@@ -3,26 +3,6 @@ const Observe = require("./utils/Observe")
 const storage = require("electron-json-storage")
 const path = require("path")
 
-import { ipcRenderer } from 'electron';
-import {
-  START_NOTIFICATION_SERVICE,
-  NOTIFICATION_SERVICE_STARTED,
-  NOTIFICATION_SERVICE_ERROR,
-  NOTIFICATION_RECEIVED as ON_NOTIFICATION_RECEIVED,
-  TOKEN_UPDATED,
-} from 'electron-push-receiver/src/constants';
-
-// Listen for service successfully started
-ipcRenderer.on(NOTIFICATION_SERVICE_STARTED, (_, token) => // do something);
-// Handle notification errors
-ipcRenderer.on(NOTIFICATION_SERVICE_ERROR, (_, error) => // do something);
-// Send FCM token to backend
-ipcRenderer.on(TOKEN_UPDATED, (_, token) => // Send token);
-// Display notification
-ipcRenderer.on(ON_NOTIFICATION_RECEIVED, (_, notification) => // display notification);
-// Start service
-ipcRenderer.send(START_NOTIFICATION_SERVICE, senderId);
-
 exports.Renderer = class Renderer {
     constructor(components) {
         this.navElement = document.querySelector("nav")
