0f512fbadd383f550078fffdaff46303fc825f5a
Benjamin Roseau <roseaubenjamin@gmail.com>
2019-07-04 18:29:06 +0200

    Version stable mais toujours pas de navigation entre menu

---
+++ main-process/pages/home.js
@@ -0,0 +1,4 @@
+ // Lets add event that will open our main page. ( From tray window )
+ ipcMain.on('demarrer-aumscan4-luxe', function() {
+     <link rel="import" href="sections/pages/home.html">
+   });
--- renderer-process/menus/menu.js
+++ renderer-process/menus/menu.js
@@ -1,9 +1,45 @@
 console.log("menu.js from renderer-process")
-// const {ipcRenderer} = require('electron')
-//
-// // Tell main process to show the menu when demo button is clicked
-// const contextMenuBtn = document.getElementById('context-menu')
-//
-// contextMenuBtn.addEventListener('click', () => {
-//   ipcRenderer.send('show-context-menu')
-// })
+
+const {ipcRenderer} = require('electron')
+
+// Tell main process to start the soft when the button is clicked
+const accueilBtn = document.getElementById('Accueil')
+accueilBtn.addEventListener('click', () => {
+  ipcRenderer.send('Accueil')
+  console.log("Accueil")
+})
+
+// Tell main process to start the soft when the button is clicked
+const sauvegardeBtn = document.getElementById('Sauvegarde')
+sauvegardeBtn.addEventListener('click', () => {
+  ipcRenderer.send('Sauvegarde')
+  console.log("Sauvegarde")
+})
+
+// Tell main process to start the soft when the button is clicked
+const comptabiliteBtn = document.getElementById('Comptabilite')
+comptabiliteBtn.addEventListener('click', () => {
+  ipcRenderer.send('Comptabilite')
+  console.log("Comptabilite")
+})
+
+// Tell main process to start the soft when the button is clicked
+const proposBtn = document.getElementById('Propos')
+proposBtn.addEventListener('click', () => {
+  ipcRenderer.send('Propos')
+  console.log("Propos")
+})
+
+// Tell main process to start the soft when the button is clicked
+const aideBtn = document.getElementById('Aide')
+aideBtn.addEventListener('click', () => {
+  ipcRenderer.send('Aide')
+  console.log("Aide")
+})
+
+// Tell main process to start the soft when the button is clicked
+const depannageBtn = document.getElementById('Depannage')
+depannageBtn.addEventListener('click', () => {
+  ipcRenderer.send('Depannage')
+  console.log("Depannage")
+})
