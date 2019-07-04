0520e2192d65011749537b2dda289ccd3d4ddd39
Benjamin Roseau <roseaubenjamin@gmail.com>
2019-07-04 16:10:42 +0200

    ajout de IPC sur les boutons demarrer logiciel

--- renderer-process/pages/home.js
+++ renderer-process/pages/home.js
@@ -1,9 +1,38 @@
 console.log("home.js from renderer-process")
-// const {ipcRenderer} = require('electron')
-//
-// // Tell main process to show the menu when demo button is clicked
-// const contextAboutBtn = document.getElementById('about')
-//
-// contextAboutBtn.addEventListener('click', () => {
-//   ipcRenderer.send('show-about')
-// })
+
+const {ipcRenderer} = require('electron')
+
+// Tell main process to start the soft when the button is clicked
+const aumscan4LuxeBtn = document.getElementById('aumscan4-luxe')
+aumscan4LuxeBtn.addEventListener('click', () => {
+  ipcRenderer.send('demarrer-aumscan4-luxe')
+  console.log("demarrer-aumscan4-luxe")
+})
+
+// Tell main process to start the soft when the button is clicked
+const aumscan4Btn = document.getElementById('aumscan4')
+aumscan4Btn.addEventListener('click', () => {
+  ipcRenderer.send('demarrer-aumscan4')
+  console.log("demarrer-aumscan4")
+})
+
+// Tell main process to start the soft when the button is clicked
+const aumscan3Btn = document.getElementById('aumscan3')
+aumscan3Btn.addEventListener('click', () => {
+  ipcRenderer.send('demarrer-aumscan3')
+  console.log("demarrer-aumscan3")
+})
+
+// Tell main process to start the soft when the button is clicked
+const cardiaumNaturoBtn = document.getElementById('cardiaum-naturo')
+cardiaumNaturoBtn.addEventListener('click', () => {
+  ipcRenderer.send('demarrer-cardiaum-naturo')
+  console.log("demarrer-cardiaum-naturo")
+})
+
+// Tell main process to start the soft when the button is clicked
+const cardiaumOrientBtn = document.getElementById('cardiaum-orient')
+cardiaumOrientBtn.addEventListener('click', () => {
+  ipcRenderer.send('demarrer-cardiaum-orient')
+  console.log("demarrer-cardiaum-orient")
+})
