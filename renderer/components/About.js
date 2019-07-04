3c7bdb867c6729897fa7dbd02266ce245844fcef
Benjamin Roseau <roseaubenjamin@gmail.com>
2019-07-04 11:15:05 +0200

    Ajout logo dans le menu, ajout de contenu dans About

--- renderer/components/About.js
+++ renderer/components/About.js
@@ -1,18 +1,58 @@
 module.exports = class About extends (require("../Component")) {
     render() {
         return `
-        <h1>electron-bootstrap-starter</h1>
-        <p>A starter project to create an electron app with Bootstrap 4.</p>
-        <p><b>License:</b> MIT; &copy; 2018 <a target="_blank" href="https://shaack.com">shaack.com</a>, all rights reserved</p>
-        <h2>Features</h2>
-        <ul>
-        <li>Bootstrap 4</li>
-        <li>Component architecture</li>
-        <li>Simple reactive behaviour (without React, Angular etc.)</li>
-        <li>Font-Awesome 5 integration</li>
-        <li>CRUD example with JSON storage in file</li>
-        <li>Settings component with persistent storage</li>
-        </ul>
+        <h1>À venir :</h1>
+        <div class="row">
+          <div class="col-4">
+            <div class="list-group" id="list-tab" role="tablist">
+              <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Page "Accueil"</a>
+              <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Page "Installation"</a>
+              <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Page "Aide"</a>
+              <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Page "Comptabilité"</a>
+            </div>
+          </div>
+          <div class="col-8">
+            <div class="tab-content" id="nav-tabContent">
+              <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">Lien vers le site BRSEU</li>
+                  <li class="list-group-item">Lien vers le site BRSFR</li>
+                  <li class="list-group-item">Lien vers les formations des appareils</li>
+                  <li class="list-group-item">Lien vers la formation TQ2022</li>
+                  <li class="list-group-item">Lien vers le sit</li>
+                </ul>
+              </div>
+              <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">Cras justo odio</li>
+                  <li class="list-group-item">Dapibus ac facilisis in</li>
+                  <li class="list-group-item">Morbi leo risus</li>
+                  <li class="list-group-item">Porta ac consectetur ac</li>
+                  <li class="list-group-item">Vestibulum at eros</li>
+                </ul>
+              </div>
+              <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">Cras justo odio</li>
+                  <li class="list-group-item">Dapibus ac facilisis in</li>
+                  <li class="list-group-item">Morbi leo risus</li>
+                  <li class="list-group-item">Porta ac consectetur ac</li>
+                  <li class="list-group-item">Vestibulum at eros</li>
+                </ul>
+              </div>
+              <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">Protégé la page avec un mot de passe</li>
+                  <li class="list-group-item">Calcul de financement de l'appareil</li>
+                  <li class="list-group-item">Calcul de rentabilité de l'appareil</li>
+                  <li class="list-group-item">Afficher les paiements Stripe</li>
+                  <li class="list-group-item">Afficher les paiements TransferWise</li>
+                  <li class="list-group-item">Afficher les documents VosFactures</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
         `
     }
 }
