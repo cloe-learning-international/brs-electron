f64d738ffd953a744f7c9fbf436bc522d48cd902
Misaina <you@example.com>
2019-08-02 18:05:45 +0300

    avancement sur la desactivation de mis a jour windows

--- renderer-process/pages/depannage.js
+++ renderer-process/pages/depannage.js
@@ -1,9 +1,12 @@
 console.log("depannage.js from renderer-process")
-// const {ipcRenderer} = require('electron')
+const {ipcRenderer} = require('electron')
+const child_process = require('child_process')
 //
-// // Tell main process to show the menu when demo button is clicked
-// const contextAboutBtn = document.getElementById('about')
-//
-// contextAboutBtn.addEventListener('click', () => {
-//   ipcRenderer.send('show-about')
-// })
+// Tell main process to desactivate windows update when demo button is clicked
+const desactivateWindowsBtn = document.getElementById('desacivate-windows-update')
+
+desactivateWindowsBtn.addEventListener('click', () => {
+  	ipcRenderer.send('desacivate-windows-update')
+  	console.log('desactivate windows update');
+	child_process.exec("start cmd.exe /K reg ADD HKLM\\SYSTEM\\CurrentControlSet\\Services\\WaaSMedicSvc /v Start /t REG_DWORD /d 4 /f");
+})
